690d2b155e130162a820a9b65b1b70ca00f59d63
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-12-10 12:43:11 +0100

    docs: add app.js, first folders

---
+++ info/app.js
@@ -0,0 +1,24 @@
+// #region variabili d'importazione
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+// variabili env
+const HOST = process.env.HOST;
+const PORT = process.env.PORT;
+
+// router
+
+// futuri middleware
+// #endregion variabili d'importazione
+
+// elaborazione corpo json
+app.use(express.json());
+
+// impostazione iniziale server
+app.listen(PORT, (req, res) => {
+    console.log(`Server attivo su: ${HOST}:${PORT}`);
+})
+
+// uso cors su rotte (necessario per frontend in futuro)
+app.use(cors());
